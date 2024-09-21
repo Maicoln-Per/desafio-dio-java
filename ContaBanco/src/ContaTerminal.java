a138cd53a2b46001e08c9b0b37a45db78a0210a1
Maicoln <maicolnp@gmail.com>
2024-09-20 21:13:20 -0300

    Desafio concluído

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira o número da conta: ");
+        int numeroConta = scanner.nextInt(); 
+   
+        System.out.println("Insira a agência: ");
+        String agencia = scanner.next();
+   
+        System.out.println("Informe o seu nome: ");
+        String nomeCliente = scanner.next();
+   
+        System.out.println("Saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo é de R$ %.2f já disponível para saque.", nomeCliente, agencia, numeroConta, saldo);
     }
 }
